13a20853f7c932f5934b9867327fcfe569303c72
Yan Molodtsov <yan@molodtsov.info>
2021-03-30 23:32:19 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.buttonAccept = new System.Windows.Forms.Button();
+            this.buttonDecline = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(13, 13);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(165, 32);
+            this.buttonDraw.TabIndex = 0;
+            this.buttonDraw.Text = "Нарисовать";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 20;
+            this.listBox.Location = new System.Drawing.Point(13, 51);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(165, 104);
+            this.listBox.TabIndex = 1;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(13, 238);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(77, 26);
+            this.textBoxX.TabIndex = 2;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(96, 238);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(82, 26);
+            this.textBoxY.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(13, 270);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(165, 32);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(13, 200);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(165, 32);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(184, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(581, 430);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Location = new System.Drawing.Point(13, 162);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(165, 32);
+            this.buttonChange.TabIndex = 7;
+            this.buttonChange.Text = "Изменить";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // buttonAccept
+            // 
+            this.buttonAccept.Location = new System.Drawing.Point(13, 270);
+            this.buttonAccept.Name = "buttonAccept";
+            this.buttonAccept.Size = new System.Drawing.Size(165, 32);
+            this.buttonAccept.TabIndex = 8;
+            this.buttonAccept.Text = "Подтвердить";
+            this.buttonAccept.UseVisualStyleBackColor = true;
+            this.buttonAccept.Visible = false;
+            this.buttonAccept.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // buttonDecline
+            // 
+            this.buttonDecline.Location = new System.Drawing.Point(13, 308);
+            this.buttonDecline.Name = "buttonDecline";
+            this.buttonDecline.Size = new System.Drawing.Size(165, 32);
+            this.buttonDecline.TabIndex = 9;
+            this.buttonDecline.Text = "Отмена";
+            this.buttonDecline.UseVisualStyleBackColor = true;
+            this.buttonDecline.Visible = false;
+            this.buttonDecline.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 455);
+            this.Controls.Add(this.buttonDecline);
+            this.Controls.Add(this.buttonAccept);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.buttonDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.Button buttonAccept;
+        private System.Windows.Forms.Button buttonDecline;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Dots.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+        }
+
+        private Graphics G;
+
+        private List<PointF> Arr = new List<PointF>();
+
+        private Pen Gpen = new Pen(Color.Red);
+
+        private Pen Dots = new Pen(Color.Blue);
+
+        private int Fuctorial(int n)
+        {
+            int res = 1;
+            for (int i = 1; i <= n; i++)
+                res *= i;
+            return res;
+        }
+
+        private float polinom(int i, int n, float t)
+        {
+            return (Fuctorial(n) / (Fuctorial(i) * Fuctorial(n - i))) *
+                (float)Math.Pow(t, i) * (float)Math.Pow(1 - t, n - i);
+        }
+
+        private void Draw()
+        {
+            int j = 0;
+            float step = 0.01f;
+
+            PointF[] result = new PointF[101];
+
+            for (float t = 1; t > 0; t -= step)
+            {
+                float ytmp = 0;
+                float xtmp = 0;
+                for (int i = 0; i < Arr.Count; i++)
+                {
+                    float b = polinom(i, Arr.Count - 1, t);
+                    xtmp += Arr[i].X * b;
+                    ytmp += Arr[i].Y * b;
+                }
+                result[j] = new PointF(xtmp, ytmp);
+                j++;
+            }
+            for (int i = j; i < 101; i++)
+                result[i] = result[j - 1];
+            G.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            G.ScaleTransform(1, -1);
+            pictureBox1.Refresh();
+            G.DrawLines(Gpen, result);
+            G.DrawLines(Dots, Arr.ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox.Items.Count >= 3)
+            {
+                G = Graphics.FromHwnd(pictureBox1.Handle);
+                G.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                Draw();
+            }
+            else
+                MessageBox.Show("Недостаточно точек", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((textBoxX.Text != "") && Regex.IsMatch(textBoxX.Text, @"^[-+]?[\d]*[,]?[0-9]+(?:[eE][-+]?[\d]+)?$") &&
+                (textBoxY.Text != "") && Regex.IsMatch(textBoxY.Text, @"^[-+]?[\d]*[,]?[0-9]+(?:[eE][-+]?[\d]+)?$"))
+            {
+                PointF newPoint = new PointF(float.Parse(textBoxX.Text), float.Parse(textBoxY.Text));
+                listBox.Items.Add(newPoint);
+                Arr.Add(newPoint);
+            }
+            else
+                MessageBox.Show("Ошибка при вводе данных", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox.SelectedIndex >= 0)
+            {
+                Arr.RemoveAt(listBox.SelectedIndex);
+                listBox.Items.RemoveAt(listBox.SelectedIndex);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox.SelectedIndex >= 0)
+            {
+                textBoxX.Text = Convert.ToString(Arr.ElementAt(listBox.SelectedIndex).X);
+                textBoxY.Text = Convert.ToString(Arr.ElementAt(listBox.SelectedIndex).Y);
+                buttonChange.Visible = false;
+                buttonAdd.Visible = false;
+                buttonDelete.Enabled = false;
+                buttonDraw.Enabled = false;
+                buttonAccept.Visible = true;
+                buttonDecline.Visible = true;
+                listBox.Enabled = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if ((textBoxX.Text != "") && Regex.IsMatch(textBoxX.Text, @"^[-+]?[\d]*[,]?[0-9]+(?:[eE][-+]?[\d]+)?$") &&
+                (textBoxY.Text != "") && Regex.IsMatch(textBoxY.Text, @"^[-+]?[\d]*[,]?[0-9]+(?:[eE][-+]?[\d]+)?$"))
+            {
+                PointF newPoint = new PointF(float.Parse(textBoxX.Text), float.Parse(textBoxY.Text));
+
+                Arr.RemoveAt(listBox.SelectedIndex);
+                Arr.Insert(listBox.SelectedIndex, newPoint);
+                listBox.Items.Insert(listBox.SelectedIndex, newPoint);
+                listBox.Items.RemoveAt(listBox.SelectedIndex);
+                listBox.Refresh();
+                buttonChange.Visible = true;
+                buttonAdd.Visible = true;
+                buttonDelete.Enabled = true;
+                buttonDraw.Enabled = true;
+                buttonAccept.Visible = false;
+                buttonDecline.Visible = false;
+                listBox.Enabled = true;
+            }
+            else
+                MessageBox.Show("Ошибка при вводе данных", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBoxX.Text = "";
+            textBoxY.Text = "";
+            buttonChange.Visible = true;
+            buttonAdd.Visible = true;
+            buttonDelete.Enabled = true;
+            buttonDraw.Enabled = true;
+            buttonAccept.Visible = false;
+            buttonDecline.Visible = false;
+            listBox.Enabled = true;
+        }
+    }
+}
